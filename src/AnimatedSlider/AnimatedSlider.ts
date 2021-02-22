cc230e607f5ce59eca7692bd3fee0d0e9335f079
Mohammed Ramadan <mohammed.ramadanko@gmail.com>
2021-02-22 03:11:43 +0200

    fix(component): fix text selection & animation transition for AnimatedSlider (#18)

--- src/AnimatedSlider/AnimatedSlider.ts
+++ src/AnimatedSlider/AnimatedSlider.ts
@@ -36,7 +36,7 @@ class AnimatedSlider extends FadeSlider {
     currentOverlay.className += ' animate-items'
     setTimeout(() => {
       currentOverlay.remove()
-    }, this.options.animationSpeed * 1000 )
+    }, this.options.animationSpeed * 1000)
   }
 
   goToPrev(): void {
@@ -58,50 +58,58 @@ class AnimatedSlider extends FadeSlider {
     setTimeout(() => {
       this.isTransitionActive = false
       this.toggleButtonsState(false)
-    }, this.options.animationSpeed * 1000 + 100)
+    }, this.options.animationSpeed * 1000 + 500)
   }
 
+  /**
+   * @description create snapshot image for each silde
+   */
   takeSnapshot(): void {
-    if( !this.savedSlides[this.currentSlide]){
+    if (!this.savedSlides[this.currentSlide]) {
       domToImage.toBlob(this.trackContainer)
-      .then((blobImage: Blob) => {
-        const url = URL.createObjectURL(blobImage)
-        this.savedSlides[this.currentSlide] = url
-        this.imageUrl = url
-        this.createOverlay()
-      })
-    }else{
+        .then((blobImage: Blob) => {
+          const url = URL.createObjectURL(blobImage)
+          this.savedSlides[this.currentSlide] = url
+          this.imageUrl = url
+          this.createOverlay()
+        })
+    } else {
       this.imageUrl = this.savedSlides[this.currentSlide]
       this.createOverlay()
-    } 
+    }
   }
 
+  /**
+   * @description create an overlay slide item for the active to be animated
+   */
   createOverlay(): void {
     const { imageUrl, newContainer } = this;
     this.currentAnimation = this.options.animations.shift()
-    this.options.animations.push(this.currentAnimation)
+    this.options.animations = [...this.options.animations, this.currentAnimation]
     const newOverlay = document.createElement('div')
     newOverlay.className = `overlay ${this.currentAnimation}`
     const markup = animationMarkups[this.currentAnimation](imageUrl, newContainer, this.options.animationSpeed)
     newOverlay.innerHTML = markup// get animated children
     this.newContainer.appendChild(newOverlay)
-    this.animatedContainers.push(newOverlay)
-    if( !this.initialized){
+    this.animatedContainers = [...this.animatedContainers, newOverlay];
+    if (!this.initialized) {
       this.enableInteractions()
     }
-
   }
 
+  /**
+   * called by init function from BasicSlider class
+   */
   prepareAnimation(): void {
     this.takeSnapshot()
   }
 
-  enableInteractions(): void{
+  enableInteractions(): void {
     setTimeout(() => {
       this.initialized = true
       this.toggleButtonsState(false)
       this.isTransitionActive = false
-    },0)
+    }, 0)
   }
 
 }
