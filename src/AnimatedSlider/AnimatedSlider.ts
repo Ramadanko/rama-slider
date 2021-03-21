e622c9b8b2257276aa8681327c26bc87a6313eff
Mohammed Ramadan <mohammed.ramadanko@gmail.com>
2021-03-21 04:13:34 +0200

    Dynamic css files 2 (#20)
    * fix(component): fix text selection & animation transition for AnimatedSlider
    
    * fix: fix type of inline css property
    
    * refactor: save changes
    
    * style: npm audit fix
    
    * perf: use inline css styles for snapshots and use image preloader to load snapshot before transition
    
    * feat(add new animation stripesoutsequential): new animation plus code refactoring and add prettierrc

--- src/AnimatedSlider/AnimatedSlider.ts
+++ src/AnimatedSlider/AnimatedSlider.ts
@@ -1,40 +1,40 @@
-import '../index.scss'
-import './AnimatedSlider.scss'
-import FadeSlider from '../FadeSlider/FadeSlider'
-import domToImage from 'dom-to-image'
-import animationMarkups from './AnimationMarkups'
-import OptionsInterface from '../BasicSlider/OptionsInterface'
+import '../index.scss';
+import './AnimatedSlider.scss';
+import FadeSlider from '../FadeSlider/FadeSlider';
+import domToImage from 'dom-to-image';
+import animationMarkups from './AnimationMarkups';
+import OptionsInterface from '../BasicSlider/OptionsInterface';
 
 class AnimatedSlider extends FadeSlider {
-
-  imageUrl: string
-  overlay: HTMLElement
-  currentAnimation: string
-  animatedContainers: Array<HTMLElement> = []
-  initialized = false
+  imageUrl: string;
+  overlay: HTMLElement;
+  currentAnimation: string;
+  animatedContainers: Array<HTMLElement> = [];
+  initialized = false;
   imagePreload = document.createElement('img');
+  interval: number;
   constructor(elementClassOrId: string, options: OptionsInterface) {
-    super(elementClassOrId, options)
-    this.isTransitionActive = true
-    this.toggleButtonsState(true)
-    this.insertImagePreloader()
+    super(elementClassOrId, options);
+    this.isTransitionActive = true;
+    this.toggleButtonsState(true);
+    this.insertImagePreloader();
   }
 
   createSliderHtmlClass(): string {
-    return 'rama-slider-animated'
+    return 'rama-slider-animated';
   }
 
   getTransitionDuration(): number {
-    return 0
+    return 0;
   }
 
   /**
    * @description create image preloader to load snapshot images before animation transition
    */
   insertImagePreloader(): void {
-    const styles = `position: absolute; z-index: -100; left: -100%; visibility:hidden`
-    this.imagePreload.setAttribute('style', styles)
-    document.body.appendChild(this.imagePreload)
+    const styles = `position: absolute; z-index: -100; left: -100%; visibility:hidden`;
+    this.imagePreload.setAttribute('style', styles);
+    document.body.appendChild(this.imagePreload);
   }
 
   /**
@@ -44,8 +44,8 @@ class AnimatedSlider extends FadeSlider {
   loadSnapshotImage(url: string): void {
     this.imagePreload.onload = () => {
       this.createOverlay();
-    }
-    this.imagePreload.src = url
+    };
+    this.imagePreload.src = url;
   }
 
   /**
@@ -53,55 +53,64 @@ class AnimatedSlider extends FadeSlider {
    * @param url String: snpashot image url
    */
   insertCssStyles(url: string): void {
-    const sliderId = this.newContainer.getAttribute('data-rama-id')
-    const cssStyle = `.overlay-number-${this.currentSlide}.${sliderId} .image-portion{background-image: url("${url}")}`
-    const style = document.createElement('style')
-    style.innerHTML = cssStyle
-    document.body.appendChild(style)
+    const sliderId = this.newContainer.getAttribute('data-rama-id');
+    const cssStyle = `.overlay-number-${this.currentSlide}.${sliderId}
+    .image-portion{background-image: url("${url}")}`;
+    const style = document.createElement('style');
+    style.innerHTML = cssStyle;
+    document.body.appendChild(style);
+  }
+
+  setNextInterval(): void {
+    let interval = this.options.animationSpeed * 1000;
+    switch (this.currentAnimation) {
+      case 'StripesOutSequential':
+        interval = interval + 4000;
+        break;
+    }
+    this.interval = interval;
   }
 
   /**
    * @description navigation to next slider
    */
   goToNext(): void {
-    if (this.isTransitionActive)
-      return
-    this.toggleTransitionState()
-    this.moveToNextItem()
-    this.prepareAnimation()
-    const currentOverlay = this.animatedContainers.shift()
-    currentOverlay.classList.add('animate-items')
+    if (this.isTransitionActive) return;
+    this.toggleTransitionState();
+    this.moveToNextItem();
+    this.prepareAnimation();
+    const currentOverlay = this.animatedContainers.shift();
+    currentOverlay.classList.add('animate-items');
     setTimeout(() => {
-      currentOverlay.remove()
-    }, this.options.animationSpeed * 1000)
+      currentOverlay.remove();
+    }, this.interval);
   }
 
   /**
    * @description navigate to previous slider
    */
   goToPrev(): void {
-    if (this.isTransitionActive)
-      return
-    this.toggleTransitionState()
-    this.moveToPrevItem()
-    this.prepareAnimation()
-    const currentOverlay = this.animatedContainers.shift()
-    currentOverlay.classList.add('animate-items')
+    if (this.isTransitionActive) return;
+    this.toggleTransitionState();
+    this.moveToPrevItem();
+    this.prepareAnimation();
+    const currentOverlay = this.animatedContainers.shift();
+    currentOverlay.classList.add('animate-items');
     setTimeout(() => {
-      currentOverlay.remove()
-    }, this.options.animationSpeed * 1000)
+      currentOverlay.remove();
+    }, this.interval);
   }
 
   /**
    * @description disable user interaction during animation transition
    */
   toggleTransitionState(): void {
-    this.isTransitionActive = true
-    this.toggleButtonsState(true)
+    this.isTransitionActive = true;
+    this.toggleButtonsState(true);
     setTimeout(() => {
-      this.isTransitionActive = false
-      this.toggleButtonsState(false)
-    }, this.options.animationSpeed * 1000 + 100)
+      this.isTransitionActive = false;
+      this.toggleButtonsState(false);
+    }, this.interval);
   }
 
   /**
@@ -109,15 +118,14 @@ class AnimatedSlider extends FadeSlider {
    */
   takeSnapshot(): void {
     if (!this.savedSlides[this.currentSlide]) {
-      domToImage.toBlob(this.trackContainer.children[this.currentSlide - 1])
-        .then((blobImage: Blob) => {
-          const url = URL.createObjectURL(blobImage)
-          this.loadSnapshotImage(url)
-          this.insertCssStyles(url)
-          this.savedSlides[this.currentSlide] = url
-        })
+      domToImage.toBlob(this.trackContainer.children[this.currentSlide - 1]).then((blobImage: Blob) => {
+        const url = URL.createObjectURL(blobImage);
+        this.loadSnapshotImage(url);
+        this.insertCssStyles(url);
+        this.savedSlides[this.currentSlide] = url;
+      });
     } else {
-      this.createOverlay()
+      this.createOverlay();
     }
   }
 
@@ -126,14 +134,17 @@ class AnimatedSlider extends FadeSlider {
    */
   createOverlay(): void {
     const { newContainer, currentSlide } = this;
-    this.currentAnimation = this.options.animations.shift()
-    this.options.animations = [...this.options.animations, this.currentAnimation]
+    this.currentAnimation = this.options.animations.shift();
+    this.options.animations = [...this.options.animations, this.currentAnimation];
+    this.setNextInterval();
 
-    const newOverlay = document.createElement('div')
-    newOverlay.className = `overlay ${this.currentAnimation} ${newContainer.getAttribute('data-rama-id')} overlay-number-${currentSlide}`
-    const animatedElements = animationMarkups[this.currentAnimation](newContainer, this.options.animationSpeed)
-    newOverlay.innerHTML = animatedElements
-    this.newContainer.appendChild(newOverlay)
+    const newOverlay = document.createElement('div');
+    newOverlay.className = `overlay ${this.currentAnimation} ${newContainer.getAttribute(
+      'data-rama-id',
+    )} overlay-number-${currentSlide}`;
+    const animatedElements = animationMarkups[this.currentAnimation](newContainer, this.options.animationSpeed);
+    newOverlay.innerHTML = animatedElements;
+    this.newContainer.appendChild(newOverlay);
     this.animatedContainers = [...this.animatedContainers, newOverlay];
   }
 
@@ -141,9 +152,9 @@ class AnimatedSlider extends FadeSlider {
    * called by init function from BasicSlider class
    */
   prepareAnimation(): void {
-    this.takeSnapshot()
+    this.takeSnapshot();
     if (!this.initialized) {
-      setTimeout(() => this.enableInteractions())
+      setTimeout(() => this.enableInteractions());
     }
   }
 
@@ -151,11 +162,10 @@ class AnimatedSlider extends FadeSlider {
    * @description enable user interaction to navigate between slides
    */
   enableInteractions(): void {
-    this.initialized = true
-    this.toggleButtonsState(false)
-    this.isTransitionActive = false
+    this.initialized = true;
+    this.toggleButtonsState(false);
+    this.isTransitionActive = false;
   }
-
 }
 
-export default AnimatedSlider
+export default AnimatedSlider;
--- src/AnimatedSlider/AnimationMarkups.ts
+++ src/AnimatedSlider/AnimationMarkups.ts
@@ -1,43 +1,77 @@
-function StripesOut(container?: HTMLElement, animationSpeed?: number): string {
-  let markup = '';
-  const styles = `
-  background-size: 1000% 100%;
-  transition-delay: .05s;
-  transition-duration:${animationSpeed}s;
-  background-position:`;
-  for (let i = 0; i < 10; i++) {
-    markup += `<div class="image-portion" style="${styles}${(i * 11.1).toFixed(1)}% 0px; float:left"></div>`
+const calculateVariables = (container: HTMLElement) => {
+  let stripesWidth: string, backgroundSize: string, imagePosition: number, counter: number;
+  if (container.offsetWidth > 400) {
+    counter = 20;
+    stripesWidth = '5%';
+    backgroundSize = '2000%';
+    imagePosition = 5.26666666;
+  } else {
+    counter = 10;
+    stripesWidth = '10%';
+    backgroundSize = '1000%';
+    imagePosition = 11.11111111;
   }
-  return markup
+  return {
+    counter,
+    stripesWidth,
+    imagePosition,
+    backgroundSize,
+  };
+};
+
+function StripesOut(container?: HTMLElement, animationSpeed?: number, sequential?: Boolean): string {
+  let { counter, stripesWidth, imagePosition, backgroundSize } = calculateVariables(container);
+  let markup = '';
+  let styles = `
+  float:left;width: ${stripesWidth};background-size: ${backgroundSize} 100%;
+  transition-duration:${animationSpeed}s`;
+  if (sequential) {
+    for (let i = 0; i < counter; i++) {
+      markup += `
+      <div class="image-portion" style="${styles};transition-delay: ${i * 0.2}s;
+      background-position:${(i * imagePosition).toFixed(1)}% 0px;"></div>`;
+    }
+  } else {
+    for (let i = 0; i < counter; i++) {
+      markup += `
+      <div class="image-portion" style="${styles};
+      background-position:${(i * imagePosition).toFixed(1)}% 0px;"></div>`;
+    }
+  }
+  return markup;
+}
+function StripesOutSequential(container?: HTMLElement, animationSpeed?: number): string {
+  return StripesOut(container, animationSpeed, true);
 }
 
 const BoxesOut = (container?: HTMLElement, animationSpeed?: number): string => {
-  let markup = '', styles = ''
-  const boxes = container.offsetHeight <= 650 ? 3 : 4
-  const boxHeight = (100 / boxes).toFixed(1)
-  const backgroundSize = '1000% ' + boxes * 100 + '%'
-  styles = `
-  background-size:${backgroundSize};
-  float:left;
-  width: 10%;
-  height:${boxHeight}%;
-  transition-duration:${animationSpeed}s;
-  transition-delay: .05s;`;
-  let x, y = 0, xCounter = 0
+  let markup = '';
+  let styles = '';
+  const boxes = container.offsetHeight <= 650 ? 3 : 4;
+  const boxHeight = (100 / boxes).toFixed(1);
+  const backgroundSize = '1000% ' + boxes * 100 + '%';
+  styles = `float:left;width: 10%;height:${boxHeight}%;background-size:${backgroundSize};
+  transition-duration:${animationSpeed}s;transition-delay: .05s;`;
+  let x,
+    y = 0,
+    xCounter = 0;
   for (let i = 0; i < boxes * 10; i++) {
     if (i % 10 === 0 && i !== 0) {
-      xCounter = 0
-      y = boxes === 3 ? y + 50 : y + 33.33333333
+      xCounter = 0;
+      y = boxes === 3 ? y + 50 : y + 33.33333333;
     }
-    x = (xCounter * 11.1).toFixed(1) + '%'
-    markup += `<div class="image-portion" style="${styles} background-position: ${x} ${y}%"></div>`
+    x = (xCounter * 11.1).toFixed(1) + '%';
+    //styles = styles.replace(/[\n|\r|\s]*/g, '');
+    markup += `<div class="image-portion"
+    style="${styles} background-position: ${x} ${y}%"></div>`;
     xCounter++;
   }
 
-  return markup
-}
+  return markup;
+};
 
 export default {
   StripesOut,
-  BoxesOut
-}
+  BoxesOut,
+  StripesOutSequential,
+};
